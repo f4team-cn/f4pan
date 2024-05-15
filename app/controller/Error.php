4278f3d1458c1f908040e3e25e51e6355a952434
Yincmewy <2025173673@qq.com>
2024-05-16 03:26:29 +0800

    miss路由新增author,版本号,github,website

--- app/controller/Error.php
+++ app/controller/Error.php
@@ -8,6 +8,6 @@ class Error extends BaseController
 {
     public function index(): \think\Response
     {
-        return responseJson(-1, '接口不存在');
+        return responseJson(-1, '接口不存在', ['author'=>'F4Team', 'version'=>env('app.version'), 'github'=> 'https://github.com/f4team-cn/f4pan', 'website'=>'https://www.f4team.cn/']);
     }
 }
